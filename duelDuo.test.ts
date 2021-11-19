4d12614973448503806424592cefca34e1cfc0b6
Blake Pond <blakehp@icloud.com>
2021-11-19 12:04:03 -0700

    final commit

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -17,4 +17,12 @@ test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
+    await driver.sleep(1000)
+})
+
+test('select the draw button', async () => {
+    const draw = await driver.findElement(By.id('draw'))
+    const clicked = draw.click()
+    await driver.sleep(3000)
+    expect(clicked).toBe(true)
 })
--- heroku.md
+++ heroku.md
@@ -1,4 +1,7 @@
 ## Paste your link below
-
+<!-- heroku app -->
 https://week6-assessment-bp.herokuapp.com/
+
+<!-- trello manual test page -->
+https://trello.com/b/wFxERa61/week6assessment
 
